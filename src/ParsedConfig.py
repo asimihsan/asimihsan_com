0c6bbb6a0fef678288c86f636cb07c20b9c81a91
Asim Ihsan <asim.ihsan@gmail.com>
2013-02-20 15:18:08 +0000

    asimihsan_com - build scripts can GZIP all HTML/CSS/JS, upload to S3, invalidate on cloudfront.

--- src/ParsedConfig.py
+++ src/ParsedConfig.py
@@ -6,14 +6,34 @@ class ParsedConfig(object):
         with open(config_filepath) as f:
             config = yaml.safe_load(f)
         current_directory = os.path.abspath(os.path.join(config_filepath, os.pardir))
-        self._source_directory = os.path.abspath(os.path.join(current_directory, config["source_directory"]))
+        self._web_directory = os.path.abspath(os.path.join(current_directory, config["web_directory"]))
         self._build_directory = os.path.abspath(os.path.join(current_directory, config["build_directory"]))
+        self._yuicompress_path = os.path.abspath(config["yuicompress_path"])
+        self._pandoc_path = os.path.abspath(config["pandoc_path"])
+        self._s3_bucket = config["s3_bucket"]
+        self._manifest_filename = config["manifest_filename"]
 
     @property
-    def source_directory(self):
-        return self._source_directory
+    def web_directory(self):
+        return self._web_directory
 
     @property
     def build_directory(self):
         return self._build_directory
+
+    @property
+    def yuicompress_path(self):
+        return self._yuicompress_path
+
+    @property
+    def pandoc_path(self):
+        return self._pandoc_path
+
+    @property
+    def s3_bucket(self):
+        return self._s3_bucket
+
+    @property
+    def manifest_filename(self):
+        return self._manifest_filename
 
--- src/compress_combine.py
+++ src/compress_combine.py
@@ -3,6 +3,14 @@
 import os
 import sys
 import subprocess
+import shutil
+import re
+import gzip
+import boto
+import contextlib
+import hashlib
+from glob import glob
+import posixpath
 
 from ParsedConfig import ParsedConfig
 
@@ -28,6 +36,114 @@ ch.setFormatter(formatter)
 logger.addHandler(ch)
 # -----------------------------------------------------------------------------
 
+def copy_web_to_build(config):
+    logger = logging.getLogger("%s.copy_web_to_build" % APP_NAME)
+    logger.debug("entry. config.web_directory: %s, config.build_directory: %s" % (config.web_directory, config.build_directory))
+
+    # -------------------------------------------------------------------------
+    #   Copy a fresh copy of the web directory to the build directory.
+    # -------------------------------------------------------------------------
+    if os.path.isdir(config.build_directory):
+        shutil.rmtree(config.build_directory)
+    shutil.copytree(config.web_directory, config.build_directory)
+    # -------------------------------------------------------------------------
+
+def compress_files_in_build_directory(config):
+    logger = logging.getLogger("%s.compress_files_in_build_directory" % APP_NAME)
+    logger.debug("entry. config.build_directory: %s" % config.build_directory)
+
+    RE_HTML = re.compile(".*\.(html|js|css)$")
+
+    for root, dirs, files in os.walk(config.build_directory):
+        for filename in files:
+            filepath = os.path.join(root, filename)
+            if not RE_HTML.search(filepath):
+                continue
+            logger.info("gzipping file: '%s'" % filepath)
+            with open(filepath) as f_in:
+                contents = f_in.read()
+            with contextlib.closing(gzip.open(filepath, "wb")) as f_out:
+                f_out.write(contents)
+
+def upload_build_directory_to_s3(config):
+    logger = logging.getLogger("%s.upload_build_directory_to_s3" % APP_NAME)
+    logger.debug("entry. config.build_directory: %s, config.s3_bucket: %s" % (config.build_directory, config.s3_bucket))
+
+    with contextlib.closing(boto.connect_s3()) as conn:
+        with contextlib.closing(boto.connect_cloudfront()) as conn_cloudfront:
+            cloudfront_distribution = [elem for elem in conn_cloudfront.get_all_distributions() if config.s3_bucket in elem.origin.dns_name][0]
+            cloudfront_distribution = cloudfront_distribution.get_distribution()
+            all_subpaths = []
+
+            bucket = conn.get_bucket(config.s3_bucket)
+            manifest_key = bucket.get_key(config.manifest_filename)
+            existing_manifest_hashes = {}
+            if not manifest_key:
+                logger.debug("manifest does not exist, upload everything.")
+            else:
+                logger.debug("manifest exists, may be duplicate files.")
+                manifest_contents = manifest_key.get_contents_as_string()
+                for line in manifest_contents.splitlines():
+                    (subpath, digest) = line.strip().split()
+                    existing_manifest_hashes[subpath] = digest
+
+            build_path_length = len(config.build_directory.split(os.sep))
+            for root, dirs, files in os.walk(config.build_directory):
+                for filename in files:
+                    filepath = os.path.join(root, filename)
+                    subpath = posixpath.join(*filepath.split(os.sep)[build_path_length:])
+                    digest = calculate_hash(filepath)
+                    if existing_manifest_hashes.get(subpath, None) == digest:
+                        logger.debug("file '%s' already exists and is identical, skipping." % subpath)
+                        continue
+                    logger.debug("uploading subpath '%s'" % subpath)
+                    all_subpaths.append(subpath)
+                    key = bucket.delete_key(subpath)
+                    key = bucket.new_key(subpath)
+                    if is_gzip_file(filepath):
+                        logger.debug("mark as a gzipped file")
+                        key.set_metadata("Content-Encoding", "gzip")
+                    key.set_contents_from_filename(filepath)
+                    key.make_public()
+
+            logger.debug("creating cloudfront invalidation request")
+            conn_cloudfront.create_invalidation_request(cloudfront_distribution.id, all_subpaths)
+
+def generate_manifest_file_for_build_directory(config):
+    logger = logging.getLogger("%s.generate_manifest_file_for_build_directory" % APP_NAME)
+    logger.debug("entry. config.build_directory: %s, config.manifest_filename: %s" % (config.build_directory, config.manifest_filename))
+
+    manifest_path = os.path.join(config.build_directory, config.manifest_filename)
+    logger.debug("manifest_path: %s" % manifest_path)
+    build_path_length = len(config.build_directory.split(os.sep))
+    with open(manifest_path, "w") as f_out:
+        for root, dirs, files in os.walk(config.build_directory):
+            for filename in files:
+                filepath = os.path.join(root, filename)
+                subpath = posixpath.join(*filepath.split(os.sep)[build_path_length:])
+                digest = calculate_hash(filepath)
+                logger.debug("subpath: %s, digest: %s" % (subpath, digest))
+                f_out.write("%s\t%s\n" % (subpath, digest))
+
+def calculate_hash(filepath, algorithm=hashlib.md5, length=16*1024):
+    m = algorithm()
+    with open(filepath) as f_in:
+        while True:
+            buf = f_in.read(length)
+            if not buf:
+                break
+            m.update(buf)
+    return m.hexdigest()
+
+def is_gzip_file(filepath):
+    try:
+        with contextlib.closing(gzip.open(filepath, "rb")) as f_in:
+            f_in.readline()
+    except IOError:
+        return False
+    else:
+        return True
+
 def main():
     logger = logging.getLogger("%s.main" % APP_NAME)
     logger.debug("entry.")
@@ -39,8 +155,10 @@ def main():
     # -------------------------------------------------------------------------
 
     config = ParsedConfig(CONFIG_FILEPATH)
-
-    import ipdb; ipdb.set_trace()
+    copy_web_to_build(config)
+    compress_files_in_build_directory(config)
+    generate_manifest_file_for_build_directory(config)
+    upload_build_directory_to_s3(config)
 
 if __name__ == "__main__":
     main()
